35642ec31aaad993998d3fbb93115cc5876d901e
Miguel Angel Lupani <mlupani2@gmail.com>
2022-06-05 19:41:26 -0300

    videos put and delete added

--- controllers/videos.ts
+++ controllers/videos.ts
@@ -17,7 +17,6 @@ export const getVideos = async (req: Request, res: Response) => {
 };
 
 export const postVideos = async (req: Request, res: Response) => {
-  console.log(req.body);
   // TODO: guardar en storage
   try {
     const { title, description, url, duration, authorID, custodians, receptors } = req.body;
@@ -38,6 +37,43 @@ export const postVideos = async (req: Request, res: Response) => {
     console.log(error);
     res.status(500).json({
       message: 'Error al crear el video'
+    });
+  }
+};
+
+export const updateVideo = async (req: Request, res: Response) => {
+  try {
+    const { title, description, url, duration, authorID, custodians, receptors } = req.body;
+    const video = await Video.findByIdAndUpdate(req.params.id, {
+      title,
+      description,
+      url,
+      duration,
+      authorID,
+      custodians,
+      receptors
+    }, { new: true });
+    res.json({
+      video
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error al actualizar el video'
+    });
+  }
+};
+
+export const deleteVideo = async (req: Request, res: Response) => {
+  try {
+    const video = await Video.findByIdAndDelete(req.params.id);
+    res.json({
+      video
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error al eliminar el video'
     });
   }
 };
--- routes/video.ts
+++ routes/video.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getVideos, postVideos } from '../controllers/videos';
+import { getVideos, postVideos, updateVideo, deleteVideo } from '../controllers/videos';
 
 const router = Router();
 
 router.get('/', getVideos);
 router.post('/', postVideos);
+router.put('/:id', updateVideo);
+router.delete('/:id', deleteVideo);
 
 module.exports = router;
