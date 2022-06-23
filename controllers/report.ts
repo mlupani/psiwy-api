da1fe5e1e0e0687d95d9094626004a84e8e24281
Miguel Angel Lupani <mlupani2@gmail.com>
2022-06-23 12:20:00 -0300

    changes in credentials google cloud

--- controllers/report.ts
+++ controllers/report.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-const { Video } = require('../models');
+// const { Video } = require('../models');
 const { sendMail } = require('../helpers/send-mail');
 
 export const report = async (req: Request, res: Response) => {
   try {
+    /*
     const { id } = req.body;
     const video = await Video.findById(id);
     if (!video) {
@@ -16,6 +17,8 @@ export const report = async (req: Request, res: Response) => {
     receptors.forEach(({ email, userName }: {email: string, userName: string}) => {
       sendMail(email, `Aviso de evento ${userName}`, `el video es ${video.title} y su id es ${video.id}, su url publica es ${video.url}`);
     });
+    */
+    sendMail('mlupani2@gmail.com', 'aviso de evento', 'contenido');
   } catch (error) {
     res.status(500).json({ message: error });
   }
