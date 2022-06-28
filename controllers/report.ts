8011d43e38cb86f2a773176c6724d782e433c2c1
Miguel Angel Lupani <mlupani2@gmail.com>
2022-06-28 15:10:20 -0300

    deleting send mail extrag

--- controllers/report.ts
+++ controllers/report.ts
@@ -4,7 +4,6 @@ const { sendMail } = require('../helpers/send-mail');
 
 export const reportDate = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.body);
     const { id } = req.body;
     const video = await Video.findById(id);
     if (!video) {
@@ -14,11 +13,15 @@ export const reportDate = async (req: Request, res: Response, next: NextFunction
     }
     const { receptors } = video;
 
+    console.log({ video, receptors });
+
     receptors.forEach(({ email, userName }: {email: string, userName: string}) => {
       sendMail(email, `Aviso de evento ${userName}`, `el video es ${video.title} y su id es ${video.id}, su url publica es ${video.url}`);
     });
 
-    sendMail('mlupani2@gmail.com', 'Aviso de evento', 'descripcion');
+    res.json({
+      message: 'Email sent'
+    });
   } catch (error) {
     res.status(500).json({ message: error });
   }
